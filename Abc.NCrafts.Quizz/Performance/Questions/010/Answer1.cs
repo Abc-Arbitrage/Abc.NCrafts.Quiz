5739ad61eeafa9ef8442113b874f9511c3efe63c
Olivier Coanet <o.coanet@abc-arbitrage.com>
2016-05-09 15:34:02 +0200

    Add performance question #10

---
+++ Abc.NCrafts.Quizz/Performance/Questions/010/Answer1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Abc.NCrafts.Quizz.Performance.Questions._010
+{
+    [CorrectAnswer(Difficulty = Difficulty.Easy)]
+    public class Answer1
+    {
+        private static readonly Dictionary<string, int> _values = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+
+        static Answer1()
+        {
+            var random = new Random();
+            foreach (var value in Enumerable.Range(0, 500).Select(_ => random.Next(5000)))
+            {
+                _values[value.ToString()] = value;
+            }
+        }
+
+        public static void Run()
+        {
+            // begin
+            var hasValue = _values.ContainsKey("555");
+            // end
+
+            Logger.Log(hasValue ? "Hooray!" : "Alas!");
+        }
+    }
+}
---
+++ Abc.NCrafts.Quizz/Performance/Questions/010/Answer2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Abc.NCrafts.Quizz.Performance.Questions._010
+{
+    public class Answer2
+    {
+        private static readonly Dictionary<string, int> _values = new Dictionary<string, int>(StringComparer.InvariantCultureIgnoreCase);
+
+        static Answer2()
+        {
+            var random = new Random();
+            foreach (var value in Enumerable.Range(0, 500).Select(_ => random.Next(5000)))
+            {
+                _values[value.ToString()] = value;
+            }
+        }
+
+        public static void Run()
+        {
+            // begin
+            var hasValue = _values.ContainsKey("555");
+            // end
+
+            Logger.Log(hasValue ? "Hooray!" : "Alas!");
+        }
+    }
+}
