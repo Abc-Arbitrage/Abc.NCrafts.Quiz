72d1f7504f9e70783b671dc40fa9d2a5e1eb247c
Romain Verdier <r.verdier@abc-arbitrage.com>
2016-05-09 18:24:49 +0200

    Add performance question #20

---
+++ Abc.NCrafts.Quizz/Performance/Questions/020/Answer1.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+
+namespace Abc.NCrafts.Quizz.Performance.Questions._020
+{
+    public class Answer1
+    {
+        private static int _counter;
+
+        public static void Run()
+        {
+            // begin
+            var tasks = new Task[20];
+            for (var i = 0; i < tasks.Length; i++)
+            {
+                tasks[i] = Task.Run(() =>
+                {
+                    for (var j = 0; j < 10 * 1000 * 1000; j++)
+                    {
+                        lock (tasks)
+                            _counter++;
+                    }
+                });
+            }
+            Task.WaitAll(tasks);
+            // end
+
+            Logger.Log($"Counter: {_counter}");
+        }
+    }
+}
---
+++ Abc.NCrafts.Quizz/Performance/Questions/020/Answer2.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Abc.NCrafts.Quizz.Performance.Questions._020
+{
+    [CorrectAnswer(Difficulty = Difficulty.Easy)]
+    public class Answer2
+    {
+        private static int _counter;
+
+        public static void Run()
+        {
+            // begin
+            var tasks = new Task[20];
+            for (var i = 0; i < tasks.Length; i++)
+            {
+                tasks[i] = Task.Run(() =>
+                {
+                    for (var j = 0; j < 10 * 1000 * 1000; j++)
+                    {
+                        Interlocked.Increment(ref _counter);
+                    }
+                });
+            }
+            Task.WaitAll(tasks);
+            // end
+
+            Logger.Log($"Counter: {_counter}");
+        }
+    }
+}
