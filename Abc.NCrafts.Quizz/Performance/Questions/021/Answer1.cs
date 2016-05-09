289d063c86b20d96971cd339b6fc9e44e28cbf89
Olivier Coanet <o.coanet@abc-arbitrage.com>
2016-05-09 18:34:54 +0200

    Add performance question #21

---
+++ Abc.NCrafts.Quizz/Performance/Questions/021/Answer1.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Abc.NCrafts.Quizz.Performance.Questions._021
+{
+    [CorrectAnswer(Difficulty = Difficulty.Hard)]
+    public class Answer1
+    {
+        public static void Run()
+        {
+            var queue = new ConcurrentQueue<int>();
+            var sum = 0;
+
+            // begin
+            var producer = Task.Run(() =>
+            {
+                foreach (var value in Enumerable.Range(0, 10000))
+                {
+                    queue.Enqueue(value);
+                }
+                queue.Enqueue(int.MaxValue);
+            });
+
+            var consumer = Task.Run(() =>
+            {
+                var spinWait = new SpinWait();
+                while (true)
+                {
+                    int value;
+                    if (!queue.TryDequeue(out value))
+                    {
+                        spinWait.SpinOnce();
+                        continue;
+                    }
+                    if (value == int.MaxValue)
+                        break;
+
+                    sum += value;
+                }
+            });
+
+            Task.WaitAll(producer, consumer);
+            // end
+
+            Logger.Log("Sum: {0}", sum);
+        }
+    }
+}
---
+++ Abc.NCrafts.Quizz/Performance/Questions/021/Answer2.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Abc.NCrafts.Quizz.Performance.Questions._021
+{
+    public class Answer2
+    {
+        public static void Run()
+        {
+            var queue = new ConcurrentStack<int>();
+            var sum = 0;
+
+            // begin
+            var producer = Task.Run(() =>
+            {
+                foreach (var value in Enumerable.Range(0, 10000))
+                {
+                    queue.Push(value);
+                }
+                queue.Push(int.MaxValue);
+            });
+
+            var consumer = Task.Run(() =>
+            {
+                var spinWait = new SpinWait();
+                while (true)
+                {
+                    int value;
+                    if (!queue.TryPop(out value))
+                    {
+                        spinWait.SpinOnce();
+                        continue;
+                    }
+                    if (value == int.MaxValue)
+                        break;
+
+                    sum += value;
+                }
+            });
+
+            Task.WaitAll(producer, consumer);
+            // end
+
+            Logger.Log("Sum: {0}", sum);
+        }
+    }
+}
