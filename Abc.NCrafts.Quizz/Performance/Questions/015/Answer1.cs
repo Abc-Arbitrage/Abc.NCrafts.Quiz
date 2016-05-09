c3b2471ce44eb90ac26a9fff13cbf17ea8d44bfe
Romain Verdier <r.verdier@abc-arbitrage.com>
2016-05-09 17:06:31 +0200

    Add performance question #15

---
+++ Abc.NCrafts.Quizz/Performance/Questions/015/Answer1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Abc.NCrafts.Quizz.Performance.Questions._015
+{
+    public class Answer1
+    {
+        private static readonly int[] _values;
+        private static readonly int _threshold;
+
+        static Answer1()
+        {
+            _values = Enumerable.Range(0, 1000 * 1000).OrderBy(_ => Guid.NewGuid()).ToArray();
+            _threshold = _values.Length / 2;
+        }
+
+        public static void Run()
+        {
+            var count = 0;
+
+            // begin
+            for (var i = 0; i < _values.Length; i++)
+            {
+                if (_values[i] > _threshold)
+                    count++;
+            }
+            // end
+
+            Logger.Log($"{count} values greater than {_threshold}");
+        }
+    }
+}
---
+++ Abc.NCrafts.Quizz/Performance/Questions/015/Answer2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace Abc.NCrafts.Quizz.Performance.Questions._015
+{
+    [CorrectAnswer(Difficulty = Difficulty.Hard)]
+    public class Answer2
+    {
+        private static readonly int[] _values;
+        private static readonly int _threshold;
+
+        static Answer2()
+        {
+            _values = Enumerable.Range(0, 1000 * 1000).ToArray();
+            _threshold = _values.Length / 2;
+        }
+
+        public static void Run()
+        {
+            var count = 0;
+
+            // begin
+            for (var i = 0; i < _values.Length; i++)
+            {
+                if (_values[i] > _threshold)
+                    count++;
+            }
+            // end
+
+            Logger.Log($"{count} values greater than {_threshold}");
+        }
+    }
+}
