494e50c5a4d68f5f9a042c1ce8f3ccb98cce3fdd
Olivier Coanet <o.coanet@abc-arbitrage.com>
2016-05-10 19:09:00 +0200

    Add performance question #27

---
+++ Abc.NCrafts.Quizz/Performance/Questions/027/Answer2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace Abc.NCrafts.Quizz.Performance.Questions._027
+{
+    public class Answer2
+    {
+        private static readonly Random _random;
+
+        static Answer2()
+        {
+            _random = new Random();
+        }
+
+        public static void Run()
+        {
+            var value = _random.Next();
+            // begin
+            var isValid = IsValid1(value) & IsValid2(value) & IsValid3(value);
+            // end
+            Logger.Log("IsValid: {0}", isValid);
+        }
+
+        private static bool IsValid1(int n)
+        {
+            return n < 100;
+        }
+
+        private static bool IsValid2(int n)
+        {
+            return n % 10 == 0;
+        }
+
+        private static bool IsValid3(int n)
+        {
+            Thread.Sleep(1);
+            return n % 3 == 0;
+        }
+    }
+}
--- Abc.NCrafts.Quizz/Program.cs
+++ Abc.NCrafts.Quizz/Program.cs
@@ -9,7 +9,7 @@ namespace Abc.NCrafts.Quizz
     {
         private static void Main(string[] args)
         {
-            RunPerformanceQuestion("008", 10 * 1000);
+            RunPerformanceQuestion("027", 10 * 1000);
             //RunPerformanceQuestion("010", 100 * 1000);
             //RunPerformanceQuestion("006", 100);
 
