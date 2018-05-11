f6fcac2a06b550ca99aeac88a9ce897cc4955311
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2018-05-11 18:17:26 +0200

    Minor tweak

--- Abc.NCrafts.Quizz/Performance2018/Questions/007/Answer2.cs
+++ Abc.NCrafts.Quizz/Performance2018/Questions/007/Answer2.cs
@@ -9,10 +9,10 @@
             var value = _value;
 
             // begin
-            value -= (value >> 1) & 0x5555555555555555;
-            value = (value & 0x3333333333333333) + ((value >> 2) & 0x3333333333333333);
-            value = (value + (value >> 4)) & 0x0f0f0f0f0f0f0f0f;
-            return (int)((value * 0x0101010101010101) >> 56);
+            value -= (value >> 1) & 0x5555555555555555UL;
+            value = (value & 0x3333333333333333UL) + ((value >> 2) & 0x3333333333333333UL);
+            value = (value + (value >> 4)) & 0x0F0F0F0F0F0F0F0FUL;
+            return (int)((value * 0x0101010101010101UL) >> 56);
             // end
         }
     }
