36d688b88ebc40edde4a2efa636f7032d68f59d3
Romain Verdier <r.verdier@abc-arbitrage.com>
2018-05-16 14:40:15 +0200

    Add question 13

---
+++ Abc.NCrafts.Quizz/Performance2018/Questions/013/Answer1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Abc.NCrafts.Quizz.Performance2018.Questions._013
+{
+    [CorrectAnswer(Difficulty = Difficulty.Easy)]
+    public class Answer1
+    {
+        public NCrafts Run()
+        {
+            // begin
+            var n = new NCrafts(Guid.Empty, DateTime.MinValue, 42m);
+            return DoSomething(ref n);
+            // end
+        }
+
+        private ref NCrafts DoSomething(ref NCrafts s) => ref s;
+
+        public struct NCrafts
+        {
+            public Guid Field1;
+            public DateTime Field2;
+            public decimal Field3;
+
+            public NCrafts(Guid field1, DateTime field2, decimal field3)
+            {
+                Field1 = field1;
+                Field2 = field2;
+                Field3 = field3;
+            }
+        }
+    }
+}
