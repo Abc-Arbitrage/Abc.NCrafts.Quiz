c87743e8f50e6fc8ad66611635ac656c7fbc4faa
Olivier Coanet <o.coanet@abc-arbitrage.com>
2023-04-24 15:10:07 +0200

    Fix loop-unrolling question

--- Abc.NCrafts.Quiz/Performance1/Level3/Q008/Answer1.cs
+++ Abc.NCrafts.Quiz/Performance1/Level3/Q008/Answer1.cs
@@ -5,25 +5,27 @@ namespace Abc.NCrafts.Quiz.Performance1.Level3.Q008
 {
     public class Answer1
     {
-        private static readonly short[] _values;
+        private static readonly int[] _values;
 
         static Answer1()
         {
             var random = new Random();
-            _values = Enumerable.Range(0, 100)
-                                .Select(x => (short)random.Next(5000))
+            _values = Enumerable.Range(0, 1_000)
+                                .Select(x => random.Next(5000))
                                 .ToArray();
         }
 
         public static void Run()
         {
             // begin
-            short max = 0;
-            for (var index = 0; index < _values.Length; index++)
+            var values = _values;
+            var max = 0;
+            for (var index = 0; index < values.Length; index++)
             {
-                max = Math.Max(max, _values[index]);
+                max = Math.Max(max, values[index]);
             }
             // end
+            
             Logger.Log($"Max: {max}");
         }
     }
--- Abc.NCrafts.Quiz/Performance1/Level3/Q008/Answer2.cs
+++ Abc.NCrafts.Quiz/Performance1/Level3/Q008/Answer2.cs
@@ -6,28 +6,35 @@ namespace Abc.NCrafts.Quiz.Performance1.Level3.Q008
     [CorrectAnswer]
     public class Answer2
     {
-        private static readonly short[] _values;
+        private static readonly int[] _values;
 
         static Answer2()
         {
             var random = new Random();
-            _values = Enumerable.Range(0, 100)
-                                .Select(x => (short)random.Next(5000))
+            _values = Enumerable.Range(0, 1_000)
+                                .Select(x => random.Next(5000))
                                 .ToArray();
         }
 
         public static void Run()
         {
             // begin
-            short max1 = 0;
-            short max2 = 0;
-            for (var index = 0; index < _values.Length; index+=2)
+            var values = _values;
+            var max1 = 0;
+            var max2 = 0;
+            var max3 = 0;
+            var max4 = 0;
+            for (var index = 0; index < values.Length; index += 4)
             {
-                max1 = Math.Max(max1, _values[index]);
-                max2 = Math.Max(max2, _values[index + 1]);
+                max1 = Math.Max(max1, values[index]);
+                max2 = Math.Max(max2, values[index + 1]);
+                max3 = Math.Max(max3, values[index + 2]);
+                max4 = Math.Max(max4, values[index + 3]);
             }
-            short max = Math.Max(max1, max2);
+
+            var max = Math.Max(Math.Max(max1, max2), Math.Max(max3, max4));
             // end
+            
             Logger.Log($"Max: {max}");
         }
     }
